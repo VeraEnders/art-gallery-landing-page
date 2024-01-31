cf748c5bf41b5098be919fbcad80f2b50abb32d2
VeraEnders <veraabramo@gmail.com>
2024-01-31 21:59:35 +0100

    add style of hero section for mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    colors: {
+      transparent: 'transparent',
+      'dark': '#151515',
+      'neutral': '#4E4D4B',
+      'accent': '#D5966B',
+      'white': '#ffffff'
+    }
   },
   plugins: [],
 }
-
